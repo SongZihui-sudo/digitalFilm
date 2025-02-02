0b1415a580ccb23be96b21d5517a515dc9c1c5e3
SongZihui-sudo <1751122876@qq.com>
2025-02-02 13:46:28 +0800

    fix: arg parse

--- app/digitalFilm.py
+++ app/digitalFilm.py
@@ -6,8 +6,6 @@ import getopt
 
 import mynet
 
-model_path = "./final_gold_200_training_600.pt"
-
 transform = transform = transforms.Compose([
   # transforms.Resize((200, 320)),
   transforms.ToTensor()
@@ -19,12 +17,14 @@ def load_model(model_path):
     model.load_state_dict(checkpoint['model_state_dict'])
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = model.to(device)
+    print("[INFO] Open model successfully!")
     return model
 
 def load_image(image_path):
     image = Image.open(image_path).convert("RGB")
     if transform:
         image = transform(image)
+    print("[INFO] Open image successfully!")
     return image
 
 def postprocess_image(tensor):
@@ -37,10 +37,10 @@ def postprocess_image(tensor):
 
 def save_image(image, save_path):
     image.save(save_path)
+    print("[INFO] Image save successfully!")
 
 def process_images(model, image):
     model.eval()
-
     with torch.no_grad():
         image = image.unsqueeze(0).cuda()
         output= model(image)
@@ -48,10 +48,11 @@ def process_images(model, image):
         return output
            
 def main(argv):
+    model_path = "./final_gold_200_training_600.pt"
     inputfile = ''
     outputfile = ''
     try:
-        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+        opts, args = getopt.getopt(argv,"vhi:o:m:",["ifile=","ofile="])
     except getopt.GetoptError:
         print("digitalFilm.py -i <inputfile> -o <outputfile>")
         sys.exit(2)
@@ -59,15 +60,24 @@ def main(argv):
         if opt == '-h':
             print("digitalFilm.py -i <inputfile> -o <outputfile>")
             sys.exit()
+        elif opt == '-v':
+            print("digitalFilm v0.0.1 SongZihui-sudo 1751122876@qq.com")
+            sys.exit()
         elif opt in ("-i", "--ifile"):
             inputfile = arg
         elif opt in ("-o", "--ofile"):
             outputfile = arg
-    
-    image = load_image(inputfile)
-    model = load_model(model_path)
-    output_image = process_images(model, image)
-    save_image(output_image, outputfile)
+        elif opt in ("-m", "-model"):
+            model_path = arg
+
+    if inputfile != '' and output_image != '':
+        image = load_image(inputfile)
+        model = load_model(model_path)
+        output_image = process_images(model, image)
+        save_image(output_image, outputfile)
+    else:
+        print("digitalFilm.py -i <inputfile> -o <outputfile>")
+        print("input path and output path.")
 
 if __name__ == "__main__":
     main(sys.argv[1:])
